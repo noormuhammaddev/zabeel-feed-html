b49c31a9bf997be9317b55b15e69ba5f5ed7d35e
noorsystems <150002040+noorsystems@users.noreply.github.com>
2023-11-16 09:28:34 +0500

    addhome page

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function(){
+  // main slider start
+  const main_slider = jQuery('#main_slider');
+  main_slider &&  jQuery(main_slider).owlCarousel({
+    loop:true,
+    nav:false,
+    dots: true,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+  })
+  // main slider end
+
+  // media slider start
+  const media_list = jQuery('#media_list');
+  media_list && media_list.owlCarousel({
+    loop:true,
+    margin:25,
+    nav:false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        993:{
+            items:3
+        }
+    }
+  })
+  // media slider end
+
+  
+})
